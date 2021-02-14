0fcbb61b0bf92ca0935ab5cc7b7a647d8dd8323a
sani-Alpha <rai.sanidhya2010@gmail.com>
2021-02-15 00:17:43 +0530

    Updated event listener

--- Guess-the-number/script.js
+++ Guess-the-number/script.js
@@ -1,9 +1,30 @@
 'use strict';
-//setting message fields
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-//setting numeric fields
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-//setting number entry field
 
+//setting random number range from 1-20
+const secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+let highScore;
+document.querySelector('.score').textContent = score;
+//adding eventlistener for the check button
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    if(!guess)
+        document.querySelector('.message').textContent = '🛑 No Number!';
+    else if(guess > secretNumber){
+        document.querySelector('.message').textContent = '📈 Guess too High!';
+        --score;
+    }
+    else if(guess < secretNumber){
+        document.querySelector('.message').textContent = '📉 Guess too Low!';
+        --score;
+    }
+    else{
+        document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('.number').textContent = guess;
+        if(score>highScore || score>0){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    }
+    document.querySelector('.score').textContent = score;
+}); 
