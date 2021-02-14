58126c9b6556f079d3bb95aec5ed63654fb8664a
sani-Alpha <rai.sanidhya2010@gmail.com>
2021-02-14 16:15:53 +0530

    QuerySelector

--- Guess-the-number/script.js
+++ Guess-the-number/script.js
@@ -1,1 +1,9 @@
 'use strict';
+//setting message fields
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+//setting numeric fields
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+//setting number entry field
+
